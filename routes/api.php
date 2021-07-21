63189a79f206eaed35e40fbb050fbaef1943b6cd
meanuel <manuellopezflorez134@gmail.com>
2021-07-21 11:30:44 -0500

    resolviendo cambios

--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,6 @@ use Illuminate\Support\Facades\Route;
 //Rutas de Auth
 require __DIR__ . '/Auth/AuthRoutes.php';
 
-//Rutas de Task
-require __DIR__ . '/Task/TaskRoutes.php';
-
-<<<<<<< HEAD
-    Route::group([
-      'middleware' => 'auth:api'
-    ], function() {
-        Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
-    });
-});
-
 Route::group([
     'prefix' => 'task'
 ], function () {
@@ -34,7 +22,5 @@ Route::group([
 
     });
 });
-=======
 //Rutas de Formularios
 require __DIR__ . '/Formulario/FormularioRoutes.php';
->>>>>>> f6862470ab82f2551e7a60b2b76ab6013cac17ca
