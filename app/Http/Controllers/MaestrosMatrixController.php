530ad5aa3f93b6c6e234639f766d7d0f05e3eb97
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-24 12:23:47 -0500

    demas

--- app/Http/Controllers/MaestrosMatrixController.php
+++ app/Http/Controllers/MaestrosMatrixController.php
@@ -26,12 +26,25 @@ class MaestrosMatrixController extends Controller
             "tabla" => "required|string"
         ]);
 
+        // permisos
         $permisos = DB::table('root_000105')->where('Tabusu', Auth::user()->Codigo)->where('Tabtab', $request->tabla)->first(['Tabcvi', 'Tabcam', 'Tabpgr', 'Tabopc']);
+
+        // detalle
+        $tabla_consecutivo = explode('_', $request->tabla);
+        $detalle = DB::table('det_formulario')->where('medico', $tabla_consecutivo[0])->where('codigo', $tabla_consecutivo[1])->where('activo', 'A')->get(['campo', 'descripcion', 'tipo', 'posicion', 'comentarios']);
+
+        // descripcion
+        $descripciones = DB::table('root_000030')->where('Dic_Usuario', $tabla_consecutivo[0])->where('Dic_Formulario', $tabla_consecutivo[1])->get(['Dic_Campo', 'Dic_Descripcion']);
+
+        // data
+        $data = DB::table($request->tabla)->get();
 
         return response()->json([
             'data' => [
                 'permisos' => $permisos,
-                
+                'detalles' => $detalle,
+                'descripciones' => $descripciones,
+                'data' => $data
             ],
             'message' => 'Retorno de datos correctos',
             'res' => 'true'
